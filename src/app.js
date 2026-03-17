d37488c708c650411e40ab14760c84a35e6c5ac9
Pratik Sangani <97470054+pratiksanganii@users.noreply.github.com>
2026-03-17 05:31:01 +0000

    Secure API with Arcjet

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes.js';
+import securityMiddleware from '#middleware/security.middleware.js';
+
 const app = express();
 
 app.use(hemet());
@@ -18,6 +20,8 @@ app.use(
     stream: { write: message => logger.info(message.trim()) },
   })
 );
+
+app.use(securityMiddleware);
 
 app.get('/', (req, res) => {
   logger.info('Hello from Acquisitions!');
